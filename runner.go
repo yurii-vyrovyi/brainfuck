5cdff1196d8d0d9be3fe688d6e603d43107b7457
Yurii Vyrovyi <y.vyrovyi@gmail.com>
2022-07-12 14:04:28 +0200

    moving runner to lib

--- runner.go
+++ runner.go
@@ -3,10 +3,13 @@ package bf_runner
 import (
 	"context"
 	"fmt"
+	"io"
 )
 
 type BfRunner struct {
-	data []DataItem
+	data   []DataItem
+	output io.Writer
+	input  io.Reader
 
 	cmdPtr  int
 	dataPtr int
@@ -24,14 +27,16 @@ type DataItem int
 
 const DefaultDataSize = 4096
 
-func New(dataSize int) *BfRunner {
+func New(dataSize int, w io.Writer, r io.Reader) *BfRunner {
 
 	if dataSize == 0 {
 		dataSize = DefaultDataSize
 	}
 
 	return &BfRunner{
-		data: make([]DataItem, dataSize),
+		data:   make([]DataItem, dataSize),
+		output: w,
+		input:  r,
 	}
 }
 
@@ -59,14 +64,70 @@ func (r *BfRunner) Run(ctx context.Context, commands string) error {
 		default:
 		}
 
-		fmt.Printf("%c", commands[r.cmdPtr])
+		if err := r.processCmd(commands[r.cmdPtr]); err != nil {
+			return fmt.Errorf("failed to process cmd [# %d]: %w", r.cmdPtr, err)
+		}
 
 		r.cmdPtr++
 	}
 }
 
 func validate(commands string) error {
-	// TODO: implement
+
+	for iCmd, cmd := range commands {
+		switch cmd {
+		case '>', '<', '+', '-', '.', ',', '[', ']':
+
+		default:
+			return fmt.Errorf(`unknown cmd [%d]: '%c'`, iCmd, cmd)
+		}
+	}
+
+	return nil
+}
+
+func (r *BfRunner) processCmd(cmd byte) error {
+
+	switch cmd {
+	case '>':
+		if r.dataPtr >= len(r.data)-1 {
+			return fmt.Errorf("shift+ out of boundary")
+		}
+		r.dataPtr++
+
+	case '<':
+		if r.dataPtr <= 0 {
+			return fmt.Errorf("shift- out of boundary")
+		}
+		r.dataPtr--
+
+	case '+':
+		r.data[r.dataPtr] += 1
+
+	case '-':
+		r.data[r.dataPtr] -= 1
+
+	case '.':
+		v := r.data[r.dataPtr]
+		if _, err := r.output.Write([]byte(fmt.Sprintf("%d", v))); err != nil {
+			return fmt.Errorf("failed to print value: %w", err)
+		}
+
+	case ',':
+		b := make([]byte, 3)
+		if _, err := r.input.Read(b); err != nil {
+			return fmt.Errorf("failed to read value: %w", err)
+		}
+
+		// TODO: unicode char to unm value
+
+	case '[':
+
+	case ']':
+
+	default:
+		return fmt.Errorf(`unknown cmd: '%c'`, cmd)
+	}
 
 	return nil
 }
